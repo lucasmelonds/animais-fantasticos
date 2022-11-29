9f0cee4403c72859a85ae2ae8d12a400654688e6
Lucas Melo <lucasmelonds@gmail.com>
2022-11-28 21:37:00 -0300

    update js

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -8,6 +8,7 @@ export default function initAnimacaoScroll() {
         const sectionTop = section.getBoundingClientRect().top
         const isSectionVisible = sectionTop - windowMetade < 0
         if (isSectionVisible) section.classList.add('ativo')
+        else if (section.classList.contains('ativo')) section.classList.remove('ativo')
       })
     }
 
